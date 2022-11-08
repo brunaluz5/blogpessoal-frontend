d9d8794855a52ab03196379d77c2ef56180cbb91
brunaluz5 <bvieiradaluz@gmail.com>
2022-11-08 10:14:36 -0300

    Atividade 2 React - Componente Home com css

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo"> HOME </h1>
+
+            <img src="https://i.imgur.com/RFOxNO7.jpeg" alt="Imagem da Tela Inicial" className="img"/>
+        </>
+    );
+}
+export default Home;
